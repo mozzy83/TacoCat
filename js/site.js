51b6d38be959d217f419d14a4ce2e8fb26d5a318
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2022-09-27 19:00:59 -0600

    My first build

--- js/site.js
+++ js/site.js
@@ -1,4 +1,31 @@
-//Call Hello World
-function HellowWorld(){
-    alert("Hello World");
+//get values
+function getValues(){
+    document.getElementById("resultArea").classList.add("invisible");
+
+    if(document.getElementById("userString").value == ""){
+        alert("Please enter a valid string")
+    }else{
+    let userString = document.getElementById("userString").value;
+    userString = userString.toLowerCase().replace(/[^a-z0-9]/g,'');
+    let revString = tacoCat(userString);
+    displayResults(userString, revString);
+    }
+}
+
+function tacoCat(userString){
+    let revString = [];
+
+    for (let index = userString.length-1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+
+return revString;
+}
+
+function displayResults(userString, revString){
+
+    if(userString == revString){
+        document.getElementById("displayResults").innerHTML = `Success!! <span class="result">${userString}</span> is a palindrome!`
+    }else { document.getElementById("displayResults").innerHTML = `<span class="result">${userString}</span> is not a palindrome.`}
+    document.getElementById("resultArea").classList.remove("invisible");
 }
